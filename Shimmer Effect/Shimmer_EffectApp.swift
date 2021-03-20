7a539bdf3c2622f7371511f99673eac3c90461b1
n0n1yzh <1160562880@qq.com>
2021-03-20 15:28:07 +0800

    Initial Commit

---
+++ Shimmer Effect/Shimmer_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shimmer_EffectApp.swift
+//  Shimmer Effect
+//
+//  Created by YZH on 2021/3/20.
+//
+
+import SwiftUI
+
+@main
+struct Shimmer_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
